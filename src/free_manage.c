8bdcf306db6f7148a7e707558a7e289430106ffa
Thi hoai thu NINH <thninh@e1r5p4.42.fr>
2017-07-22 15:16:33 +0200

    ok

--- src/free_manage.c
+++ src/free_manage.c
@@ -6,7 +6,7 @@
 /*   By: thninh <thninh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/18 10:37:14 by thninh            #+#    #+#             */
-/*   Updated: 2017/07/21 14:13:30 by thninh           ###   ########.fr       */
+/*   Updated: 2017/07/22 15:12:18 by thninh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,15 @@ void		free_file(t_elem *file)
 
 void		free_manage(t_ls **lst)
 {
-	t_ls	*tmp;
+	t_ls		*tmp;
 
 	tmp = *lst;
-	while (tmp)
+	while (*lst)
 	{
+		tmp = *lst;
+		*lst = (*lst)->next;
 		free_file(((t_elem *)tmp->data));
 		free(tmp);
-		tmp = tmp->next;
 	}
 	*lst = NULL;
 }
